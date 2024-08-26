30e24c0dd116c4137164def1fae83a809d4872ce
MinhQuan7 <leminhquanlqd711@gmail.com>
2024-08-26 23:29:18 +0700

    upgrade with canvas

--- pythoncodeCaptureIMG_Camera/requestCapture.py
+++ pythoncodeCaptureIMG_Camera/requestCapture.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, storage
 from flask import Flask, jsonify
 
 # Khởi tạo Firebase Admin SDK
-cred = credentials.Certificate("F:/EoH Company/Capture_Image_iFrame/firebase/pythoncodeCaptureIMG_Camera/captureimage-38a12-firebase-adminsdk-ngvh0-d868c69238.json")
+cred = credentials.Certificate("F:/EoH Company/Capture_Image_iFrame/firebase/pythoncodeCaptureIMG_Camera/captureimage-38a12-firebase-adminsdk-ngvh0-c454a691c9.json")
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'captureimage-38a12.appspot.com'  #projectID
 })
